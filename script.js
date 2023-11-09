14d6c5a42b49d475d1f8e807eec36c92bbfd6ec3
Rutvik <53506402+rutvik0007@users.noreply.github.com>
2023-11-10 02:05:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// You can add interactivity using JavaScript.
+// For example, you can create an image slideshow or interactive messages.
+
+// Example: Change the header background when the user hovers over it
+const header = document.querySelector('header');
+header.addEventListener('mouseover', function () {
+    header.style.background = 'linear-gradient(rgba(255, 99, 71, 0.8), rgba(255, 140, 102, 0.8)), url("header-bg2.jpg")';
+});
